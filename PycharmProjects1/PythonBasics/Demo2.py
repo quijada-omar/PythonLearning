7883da418d97873f218b0a56a4a51914617e25c0
Jesus Omar <omar.quijada4@hotmail.com>
2023-03-22 17:34:52 -0700

    Comments added

--- PycharmProjects1/PythonBasics/Demo2.py
+++ PycharmProjects1/PythonBasics/Demo2.py
@@ -1,15 +1,17 @@
+
+# First list created
 values = [1, 2, "Rahul", 3, 4]
-print(values[0])
-print(values[3])
-print(values[-1])
-print(values[1:3])
-print(values[::-1])
-values.insert(3, "Shetty")
+print(values[0]) # 1
+print(values[3]) # 3
+print(values[-1]) # 4
+print(values[1:3]) # 1-3 no countable
+print(values[::-1]) # Reversed list
+values.insert(3, "Shetty")  # 1,2,'Rahul', 'Shetty', 3, 4
 print(values)
-values.append("End")
+values.append("End") # 1, 2, "Rahul", "Shetty", 3, 4, "End"
 print(values)
-values[2] = "RAUL"
+values[2] = "RAUL"   # 1, 2, "RAUL", "Shetty", 3, 4, "End"
 print(values)
 
-del values[0]
+del values[0]   # 2, "RAUL", "Shetty", 3, 4, "End"
 print(values)
